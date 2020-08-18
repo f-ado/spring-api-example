511dd467bcc6f4d5725de97dc67c2583ba448933
AdoFelic <felic.adnan13@gmail.com>
2020-08-18 19:57:19 +0200

    Remove unused Exceptions

--- src/main/java/com/springapi/service/exception/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.springapi.service.exception;
-
-public class ServiceException extends Exception {
-    protected ServiceException(final String exceptionMessage) {
-        super(exceptionMessage);
-    }
-}
